8832242dde637a853719414b1fc7880609cc647e
Lawal Olawale <lawalolawale32@gmail.com>
2025-02-13 11:41:39 +0100

    added eslint packages

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const app = express();
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'hello world', app: 'natours' });
 // });
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Middleware');
