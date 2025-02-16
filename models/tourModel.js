16fcc4e4adea2ae4457b2c9e4b37b4a0660ce4b4
Lawal Olawale <lawalolawale32@gmail.com>
2025-02-16 14:02:02 +0100

    updated schema model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,6 +5,7 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
   rating: {
     type: Number,
@@ -14,7 +15,68 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Summary is required for a tour'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'Description is required for a tour'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have an image cover '],
+  },
+  startDates: {
+    type: [Date],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
+
+// {
+//   "id": 7,
+//   "name": "The Star Gazer",
+//   "duration": 9,
+//   "maxGroupSize": 8,
+//   "difficulty": "medium",
+//   "ratingsAverage": 4.7,
+//   "ratingsQuantity": 28,
+//   "price": 2997,
+//   "summary": "The most remote and stunningly beautiful places for seeing the night sky",
+//   "description": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+//   "imageCover": "tour-8-cover.jpg",
+//   "images": ["tour-8-1.jpg", "tour-8-2.jpg", "tour-8-3.jpg"],
+//   "startDates": ["2021-03-23,10:00", "2021-10-25,10:00", "2022-01-30,10:00"]
+// },
