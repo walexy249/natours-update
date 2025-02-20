b874e751c8ecd8d1ea4cae53f0d3b91de0b656d8
Lawal Olawale <lawalolawale32@gmail.com>
2025-02-20 23:15:51 +0100

    changed status code for global unhandled routes

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(400).json({
+  res.status(404).json({
     status: 'fail',
     message: `Can't access ${req.originalUrl} on this server`,
   });
