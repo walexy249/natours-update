28730340b50449612116ba7372f49a12e25e6158
Lawal Olawale <lawalolawale32@gmail.com>
2025-02-21 02:26:21 +0100

    implemented global error handling middleware

--- app.js
+++ app.js
@@ -36,10 +36,22 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't access ${req.originalUrl} on this server`,
-  });
+  const err = new Error(`Can't access ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't access ${req.originalUrl} on this server`,
+  // });
 });
 
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'fail';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 module.exports = app;
