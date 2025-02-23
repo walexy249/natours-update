209ec5f16238a94a068da98266b1e867ee6ab4c1
Lawal Olawale <lawalolawale32@gmail.com>
2025-02-23 04:52:43 +0100

    created different error response for production and dev environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,36 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // programming or other unknown error: don't leak to client
+    console.log('Error 🎇', err);
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something Went wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'fail';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'productions') {
+    sendErrorProd(err, res);
+  }
 };
