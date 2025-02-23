4c96a053845fba326653bcf890af5d11fc703ded
Lawal Olawale <lawalolawale32@gmail.com>
2025-02-23 06:46:48 +0100

    implemented logic for uncaught exceptions

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+process.on('uncaughtException', (err) => {
+  console.log('Error');
+  console.log(err.name, err.message);
+  server.close(() => {
+    console.log('Unhandled Rejections. Shutting down......');
+    process.exit(1);
+  });
+});
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 // console.log(process.env);
