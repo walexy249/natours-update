ce2b24d63b44f75661b017f3c1c846c8f980a95f
Lawal Olawale <lawalolawale32@gmail.com>
2025-02-23 03:32:46 +0100

    fixed bug

--- utils/appError.js
+++ utils/appError.js
@@ -1,8 +1,8 @@
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
-    this.statusCode = this.statusCode;
-    this.status = `${this.statusCode}`.startsWith('4') ? 'fail' : 'Error';
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'Error';
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
