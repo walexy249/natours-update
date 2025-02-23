97c9b25215c178b161894689db84f345ea9a2713
Lawal Olawale <lawalolawale32@gmail.com>
2025-02-23 05:36:50 +0100

    implemented logic to handle castError

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/appError');
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -24,13 +26,29 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+const handleErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'fail';
 
   if (process.env.NODE_ENV === 'development') {
+    // console.log('ERROR', err);
+    // console.log('ERROR Name', err.name);
+
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'productions') {
-    sendErrorProd(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    // let error = Object.assign({}, err);
+    let error = { ...err, name: err.name, message: err.message };
+    // console.log('ERROR', error);
+    // console.log('ERROR Name', error.name);
+
+    if (error.name === 'CastError') {
+      error = handleErrorDB(error);
+    }
+    sendErrorProd(error, res);
   }
 };
