a5b633bfb18ccc7e01a224efd0ad590e1863237e
Lawal Olawale <lawalolawale32@gmail.com>
2025-02-24 14:33:14 +0100

    implemented token signature with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,21 @@
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user,
     },
