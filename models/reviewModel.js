8ca45599753f976b8c65f9dad9fd5f615bc6fd23
Lawal Olawale <lawalolawale32@gmail.com>
2025-04-07 19:17:58 +0100

    implemented post hook to  calcAverageRating of tour when updating and deleting tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -65,8 +65,8 @@ reviewSchema.statics.calcAverageRating = async function (tourId) {
   ]);
 
   await Tour.findByIdAndUpdate(tourId, {
-    ratingsAverage: stats[0].avgRating,
-    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats.length ? stats[0].avgRating : 4.5,
+    ratingsQuantity: stats.length ? stats[0].nRating : 0,
   });
 
   console.log('stats', stats);
@@ -77,6 +77,26 @@ reviewSchema.post('save', function (doc, next) {
   this.constructor.calcAverageRating(this.tour);
 
   next();
+});
+
+// findByIdAndUpdate
+// findByIdAndDelete
+
+// The following two schema help pazs the current doc to the post hook so the calcAverageRating get triggered when updating or deleting a review
+// reviewSchema.pre(/^findOneAnd/, async function (next) {
+//   // this.r = await this.findOne().clone();
+//   // console.log('this.r', this.r);
+//   // console.log('Found document:', this.r);
+
+//   next();
+// });
+
+reviewSchema.post(/^findOneAnd/, async function (doc) {
+  console.log('Post-hook running');
+  if (doc) {
+    console.log('doc', doc);
+    await doc.constructor.calcAverageRating(doc.tour);
+  }
 });
 
 const Review = mongoose.model('Review', reviewSchema);
