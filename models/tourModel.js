6612eb58ec12738e5e4e9b15872e1c6e2bf6b96a
Lawal Olawale <lawalolawale32@gmail.com>
2025-04-22 19:03:06 +0100

    created the get distances controller for geospatial data

--- models/tourModel.js
+++ models/tourModel.js
@@ -187,12 +187,12 @@ tourSchema.pre(/^find/, function (next) {
 // });
 
 // Aggregation Middleware
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({
-    $match: { secretTour: { $ne: true } },
-  });
-  // console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({
+//     $match: { secretTour: { $ne: true } },
+//   });
+//   // console.log(this.pipeline());
+//   next();
+// });
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
